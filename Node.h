97d76c234303c322dd17348e477c743c33514792
Alexsander Bispo <35003248+Slins-23@users.noreply.github.com>
2020-03-11 08:54:33 -0300

    Add Node.h comments

--- Node.h
+++ Node.h
@@ -5,46 +5,49 @@
 #include <limits>
 #include <set>
 
+// Colors for the Nodes, based on types.
 struct colors {
-	constexpr static Uint8 passable[4] = { 0x00, 0xFF, 0x00, 0xFF };
-	constexpr static Uint8 impassable[4] = { 0xFF, 0x00, 0x00, 0xFF };
-	constexpr static Uint8 start[4] = { 0xFF, 0xFF, 0x00, 0xFF };
-	constexpr static Uint8 target[4] = { 0x00, 0xFF, 0xFF, 0xFF };
-	constexpr static Uint8 visited[4] = { 125, 5, 88, 255 };
+	constexpr static Uint8 passable[4] = { 0x00, 0xFF, 0x00, 0xFF }; // Passable - Green
+	constexpr static Uint8 impassable[4] = { 0xFF, 0x00, 0x00, 0xFF }; // Impassable - Red
+	constexpr static Uint8 start[4] = { 0xFF, 0xFF, 0x00, 0xFF }; // Start - Yellow
+	constexpr static Uint8 target[4] = { 0x00, 0xFF, 0xFF, 0xFF }; // Target - Blue
+	constexpr static Uint8 visited[4] = { 125, 5, 88, 255 }; // Visited - Purple
 };
 
+// Node class. All Nodes are instances of this class.
 #pragma once
 class Node
 {
 private:
-	bool passable;
-	bool target = false;
-	bool start = false;
-	int x;
-	int y;
-	int width = 40;
-	int height = 40;
+	bool passable; // Whether the Node can be passed through.
+	bool target = false; // If this Node is the target point.
+	bool start = false; // If this Node is the start point.
+	int x; // Node X position within the window.
+	int y; // Node Y position within the window.
+	int width = 40; // Node's width.
+	int height = 40; // Node's height.
 
-	Uint8 red;
-	Uint8 green;
-	Uint8 blue;
-	Uint8 alpha;
+	Uint8 red; // Node's red color.
+	Uint8 green; // Node's green color.
+	Uint8 blue; // Node's blue color.
+	Uint8 alpha; // Node's alpha value.
 	
-	const char* type;
+	const char* type; // Node's type. (passable || impassable || start || target || visited)
 
 public:
-	bool visited = false;
-	int cameFrom = 0;
+	bool visited = false; // Whether this Node was visited by the algorithm.
+	int cameFrom = 0; // Index of the Node from which this Node came from.
 
-	int index = 1245;
+	int index = 1245; // Node's index.
 
-	double cost = 1; // LOCAL
+	double cost = 1; // Node's cost value / How long it takes to pass through this Node
 
-	double cost_so_far = INFINITY;
+	double cost_so_far = INFINITY; // Cost from the starting point to this Node.
 
-	float fLocalGoal = INFINITY; // LOCAL
-	float fGlobalGoal = INFINITY; // GLOBAL
+	float fLocalGoal = INFINITY; // Should be cost
+	float fGlobalGoal = INFINITY; // Should be cost_so_far
 
+	// The Node constructor takes in the type of the Node and, respectively, the X and Y coordinates, relative to the display.
 	Node(const char* type, int x, int y);
 	~Node();
 	int getX();
@@ -54,10 +57,10 @@ public:
 
 
 	const char* getType();
-	Uint8 getRed() { return this->red; };
-	Uint8 getGreen() { return this->green; };
-	Uint8 getBlue() { return this->blue; };
-	Uint8 getAlpha() { return this->alpha; };
+	Uint8 getRed() { return this->red; }; // Returns the Node's red color.
+	Uint8 getGreen() { return this->green; }; // Returns the Node's green color.
+	Uint8 getBlue() { return this->blue; }; // Returns the Node's blue color.
+	Uint8 getAlpha() { return this->alpha; }; // Returns the Node's alpha value.
 
 	void setType(const char* type);
 	void setColors(const char* type);
